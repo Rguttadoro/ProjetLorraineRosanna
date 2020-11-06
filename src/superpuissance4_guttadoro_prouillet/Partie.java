ecea4db2ba19eb765366d81186c6584db96927be
Lorraine <Lorraine@DESKTOP-52IMI0I.home>
2020-11-06 17:49:10 +0100

    partie

--- src/superpuissance4_guttadoro_prouillet/Partie.java
+++ src/superpuissance4_guttadoro_prouillet/Partie.java
@@ -10,5 +10,22 @@ package superpuissance4_guttadoro_prouillet;
  * @author lprouill
  */
 public class Partie {
+    Joueur[] ListeJoeurs = new Joueur[2];
+    Jeton[] ListeJetons = new Jeton[21];
+    Joueur joueurCourant;
     
+    public void  attribuerCouleursAuxJoueurs ( ) {
+        
+        
+    }
+    
+    public void debuterPartie () {
+        
+        
+    }
+    
+    public void initialiserPartie() {
+        
+        
+    }
 }
